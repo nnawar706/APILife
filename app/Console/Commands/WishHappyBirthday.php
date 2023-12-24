31c08b8d4d8b562e5f3b9fb1704672ec4948b059
nnawar706 <nnawar706@gmail.com>
2023-12-24 19:29:53 +0600

    event update

---
+++ app/Console/Commands/WishHappyBirthday.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class WishHappyBirthday extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:wish-happy-birthday';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
