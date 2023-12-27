e71b7298c5a70234d135e8569cc12e523c4fcd50
nnawar706 <nnawar706@gmail.com>
2023-12-27 18:01:08 +0600

    dashboard data

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -7,8 +7,10 @@ use App\Models\Event;
 use App\Models\User;
 use App\Models\UserBadge;
 use App\Notifications\UserNotification;
+use Carbon\Carbon;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Spatie\Activitylog\Models\Activity;
@@ -38,13 +40,37 @@ class SystemController extends Controller
 
     public function dashboardData()
     {
-        $monthly_user_badges = DB::table('user_badges')->selectRaw(
-            'user_id, MONTH(created_at) as month_id'
-        )->groupBy('month_id','user_id')->get();
+        $data = Cache::remember('dashboard', 60*60*2, function () {
+            return $this->getDashboardData();
+        });
 
         return response()->json([
             'status' => true,
-            'data'   => $monthly_user_badges
+            'data'   => $data
         ]);
+    }
+
+    private function getDashboardData()
+    {
+        $user_badge = new UserBadge();
+        $event = new Event();
+
+        $monthly_user_badges = [];
+
+        for ($i=0; $i < 12; $i++)
+        {
+            $date      = Carbon::now()->subMonths($i);
+
+            $monthly_user_badges[$i]['month'] = Carbon::parse($date)->format('M');
+            $monthly_user_badges[$i]['user_data'] = $user_badge->clone()
+                ->whereMonth('created_at', Carbon::parse($date)->format('n'))
+                ->with('badge','user')->get();
+        }
+
+        return array(
+            'completed_events' => $event->clone()->where('event_status_id', '=', 4)->count(),
+            'ongoing_events'   => $event->clone()->where('event_status_id', '=', 1)->count(),
+            'user_badges'      => $monthly_user_badges
+        );
     }
 }
