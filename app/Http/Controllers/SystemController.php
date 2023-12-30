d91d7a1e6878c49d260c25e5b485151b410b717c
nnawar706 <nnawar706@gmail.com>
2023-12-30 20:00:14 +0600

    user badge notification with emojis

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -48,13 +48,13 @@ class SystemController extends Controller
 
     public function dashboardData()
     {
-        $data = Cache::remember('dashboard', 60*60*2, function () {
-            return $this->getDashboardData();
-        });
+//        $data = Cache::remember('dashboard', 60*60*2, function () {
+//            return $this->getDashboardData();
+//        });
 
         return response()->json([
             'status' => true,
-            'data'   => $data
+            'data'   => $this->getDashboardData()
         ]);
     }
 
