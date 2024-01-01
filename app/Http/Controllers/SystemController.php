5143230747ad53fb00706d1c4b65a00b465db9bb
nnawar706 <nnawar706@gmail.com>
2024-01-01 18:03:15 +0600

    user badge notification with emojis

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -8,6 +8,7 @@ use App\Models\Event;
 use App\Models\EventCategory;
 use App\Models\EventStatus;
 use App\Models\ExpenseBearer;
+use App\Models\ExpenseCategory;
 use App\Models\ExpensePayer;
 use App\Models\TreasurerLiability;
 use App\Models\User;
@@ -60,27 +61,30 @@ class SystemController extends Controller
 
     private function getDashboardData()
     {
-        $end_date   = Carbon::now('Asia/Dhaka');
-        $start_date = Carbon::now('Asia/Dhaka')->subMonths(1);
-        $event      = new Event();
-        $user       = User::status();
-        $user_badge = new UserBadge();
+        $end_date     = Carbon::now('Asia/Dhaka');
+        $start_date   = Carbon::now('Asia/Dhaka')->subMonths(1);
+        $start_date_week = Carbon::now('Asia/Dhaka')->subWeeks(1);
+        $eventStatus  = EventStatus::orderBy('id');
+        $user         = User::status();
+        $user_badge   = new UserBadge();
         $transactions = UserLoan::accepted();
-        $badge      = Badge::orderBy('id')->get();
+        $badge        = Badge::orderBy('id')->get();
+        $expense_categories = ExpenseCategory::leftJoin('expenses','expense_categories.id','=','expenses.expense_category_id')
+            ->leftJoin('expense_payers','expenses.id','=','expense_payers.expense_id')
+            ->leftJoin('events','expenses.event_id','=','events.id');
 
         $monthly_user_badges = [];
 
-        $date      = Carbon::now();
-
-        $monthly_user_badges['month'] = Carbon::parse($date)->format('M');
+        $monthly_user_badges['month'] = Carbon::parse($end_date)->format('M');
         $monthly_user_badges['user_data'] = $user_badge->clone()
-            ->whereMonth('created_at', Carbon::parse($date)->format('n'))
+            ->whereMonth('created_at', Carbon::parse($end_date)->format('n'))
             ->orderByDesc('point')
             ->with('badge','user')->get();
 
-        $event_count_lifetime = EventStatus::orderBy('id')->withCount('events')->get();
+        $event_count_lifetime = $eventStatus->clone()->withCount('events')->get();
 
-        $event_count_30days = EventStatus::orderBy('id')->withCount(['events' => function ($q) use ($start_date, $end_date) {
+        $event_count_30days = $eventStatus->clone()
+            ->withCount(['events' => function ($q) use ($start_date, $end_date) {
             return $q->whereBetween('created_at', [$start_date, $end_date]);
         }])->get();
 
@@ -115,32 +119,64 @@ class SystemController extends Controller
                 $user_wise_badge[$key]['badges'][$i]['badge'] = $val;
                 $user_wise_badge[$key]['badges'][$i]['count'] = $val->userBadge()->where('user_id', $item->id)->count();
             }
-//            $user_wise_badge[$key]['badges'] = $user_badge->clone()
-//                ->join('badges','user_badges.badge_id','=','badges.id')
-//                ->selectRaw('name, image_url, count(user_badges.id) as total_count')
-//                ->where('user_id', $item->id)
-//                ->groupBy('badge_id','name','image_url')
-//                ->get();
         }
 
         $event_categories = EventCategory::withCount(['events' => function ($query) {
             $query->where('event_status_id', 4);
         }])->get();
 
+        $expense_categories_lifetime = $expense_categories->clone()
+            ->selectRaw('
+                COALESCE(SUM(CASE
+                    WHEN events.event_status_id = 4 THEN expense_payers.amount
+                    ELSE 0 END
+                ), 0) AS expense_amount,
+                expense_categories.id,expense_categories.name,expense_categories.icon_url
+            ')
+            ->groupBy('expense_categories.id','expense_categories.name','expense_categories.icon_url')
+            ->get();
+
+        $expense_categories_monthly = $expense_categories->clone()
+            ->selectRaw('
+                COALESCE(SUM(CASE
+                    WHEN events.event_status_id = 4 AND
+                    expense_payers.created_at BETWEEN "'. $start_date .'" AND "'. $end_date .'" THEN expense_payers.amount
+                    ELSE 0 END
+                ), 0) AS expense_amount,
+                expense_categories.id,expense_categories.name,expense_categories.icon_url
+            ')
+            ->groupBy('expense_categories.id','expense_categories.name','expense_categories.icon_url')
+            ->get();
+
+        $expense_categories_weekly = $expense_categories->clone()
+            ->selectRaw('
+                COALESCE(SUM(CASE
+                    WHEN events.event_status_id = 4 AND
+                    expense_payers.created_at BETWEEN "'. $start_date_week .'" AND "'. $end_date .'" THEN expense_payers.amount
+                    ELSE 0 END
+                ), 0) AS expense_amount,
+                expense_categories.id,expense_categories.name,expense_categories.icon_url
+            ')
+            ->groupBy('expense_categories.id','expense_categories.name','expense_categories.icon_url')
+            ->get();
+
         return array(
-            'total_users'    => $total_users,
-            'active_users'   => $active_users,
-            'event_lifetime' => $event_count_lifetime,
-            'event_30days'   => $event_count_30days,
-            'event_categories' => $event_categories,
-            'total_mfs'      => $total_mfs,
-            'total_dues'     => $dues,
+            'total_users'                 => $total_users,
+            'active_users'                => $active_users,
+            'event_lifetime'              => $event_count_lifetime,
+            'event_30days'                => $event_count_30days,
+            'event_categories'            => $event_categories,
+            'total_mfs'                   => $total_mfs,
+            'total_dues'                  => $dues,
             'transaction_lifetime_count'  => $transaction_count,
             'transaction_lifetime_amount' => $transaction_amount,
             'transaction_30days_count'    => $transaction_count_30days,
             'transaction_30days_amount'   => $transaction_amount_30days,
             'current_month_badges'        => $monthly_user_badges,
-            'user_badges'                 => $user_wise_badge
+            'user_badges'                 => $user_wise_badge,
+            'expense_categories_lifetime' => $expense_categories_lifetime,
+            'expense_categories_monthly'  => $expense_categories_monthly,
+            'expense_categories_weekly'   => $expense_categories_weekly
         );
     }
 
