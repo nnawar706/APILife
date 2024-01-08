3e5cc10032195cef793c9457abf1884c7522b169
nnawar706 <nnawar706@gmail.com>
2024-01-08 14:20:17 +0600

    user badge notification with emojis

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://lifeplus.selopian.us'],
+    'allowed_origins' => ['*'],
+
+//    'allowed_origins' => ['https://lifeplus.selopian.us'],
 
     'allowed_origins_patterns' => [],
 
