a8702bad7799e79931f7d3533ab9d51c56880d6c
nnawar706 <nnawar706@gmail.com>
2024-01-13 10:06:11 +0600

    user badge notification with emojis

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:send-push-notification')->everyMinute();
-        $schedule->command('app:send-random-notification')->dailyAt('11:00')->timezone('Asia/Dhaka');
+//        $schedule->command('app:send-random-notification')->dailyAt('11:00')->timezone('Asia/Dhaka');
         $schedule->command('app:wish-happy-birthday')->dailyAt('0:01')->timezone('Asia/Dhaka');
         $schedule->command('app:assign-user-point')->dailyAt('0:01')->timezone('Asia/Dhaka');
         $schedule->command('app:notify-pending-loan-payable')->weekly();
