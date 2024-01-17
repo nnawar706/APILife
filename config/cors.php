f2ca5e2315392341e9f184ccebc4685e1adf16e2
nnawar706 <nnawar706@gmail.com>
2024-01-17 18:51:25 +0600

    cors restricted

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
-//    'allowed_origins' => ['https://lifeplus.selopian.us'],
+    'allowed_origins' => ['https://lifeplus.selopian.us'],
 
     'allowed_origins_patterns' => [],
 
