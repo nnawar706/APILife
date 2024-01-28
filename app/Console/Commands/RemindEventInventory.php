2ab526b5be16c06e8ffceb8d34dd9042a9d39809
nnawar706 <nnawar706@gmail.com>
2024-01-28 14:47:45 +0600

    removed inventory participants

---
+++ app/Console/Commands/RemindEventInventory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\EventInventory;
+use App\Notifications\UserNotification;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class RemindEventInventory extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:remind-event-inventory';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $eventsTomorrow = EventInventory::whereHas('event', function ($q) {
+            return $q->whereDate('from_date', Carbon::today('Asia/Dhaka')->addDay());
+        })->where('approval_status', 1)->get();
+
+        foreach ($eventsTomorrow as $item)
+        {
+            $item->assignedToInfo->notify(new UserNotification(
+                'pages/update-vaganza/' . $item->event_id,
+                "Don't forget to bring " . $item->title . ' for ' . $item->event->title . '.',
+                'Life++',
+                null
+            ));
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('app:remind-extravaganza-payment')->dailyAt('11:00')->timezone('Asia/Dhaka');
         $schedule->command('app:wish-happy-birthday')->dailyAt('0:01')->timezone('Asia/Dhaka');
         $schedule->command('app:assign-user-point')->dailyAt('0:01')->timezone('Asia/Dhaka');
+        $schedule->command('app:remind-event-inventory')->dailyAt('22:00')->timezone('Asia/Dhaka');
         $schedule->command('app:notify-pending-loan-payable')->weekly();
         $schedule->command('app:send-random-notification')->monthly();
         $schedule->command('app:assign-user-badge')->monthly();
