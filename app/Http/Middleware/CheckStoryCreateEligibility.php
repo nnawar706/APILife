0454f98cf7b59f4075ffc244eb4e024c1f832606
nnawar706 <nnawar706@gmail.com>
2024-02-05 14:03:39 +0600

    middleware unchecked for test purpose

--- app/Http/Middleware/CheckStoryCreateEligibility.php
+++ app/Http/Middleware/CheckStoryCreateEligibility.php
@@ -18,34 +18,36 @@ class CheckStoryCreateEligibility
     {
         $curTime = Carbon::now('Asia/Dhaka');
 
-        $officeStarts = Carbon::createFromTime(10); // 10 am
-        $officeEnds   = Carbon::createFromTime(18); // 6 pm
+        // TODO: uncomment this after one week
 
-        if (in_array($request->ip(), ['103.205.71.148', '2400:3240:900a:17::1008', '127.0.0.1']))
-        {
-            return response()->json([
-                'status' => false,
-                'error'  => 'Currently in office? Try to concentrate on your pending works.'
-            ], Response::HTTP_LOCKED);
-        }
+//        $officeStarts = Carbon::createFromTime(10); // 10 am
+//        $officeEnds   = Carbon::createFromTime(18); // 6 pm
+//
+//        if (in_array($request->ip(), ['103.205.71.148', '2400:3240:900a:17::1008', '127.0.0.1']))
+//        {
+//            return response()->json([
+//                'status' => false,
+//                'error'  => 'Currently in office? Try to concentrate on your pending works.'
+//            ], Response::HTTP_LOCKED);
+//        }
+//
+//        if (!$curTime->clone()->isFriday() && $curTime->clone()->between($officeStarts, $officeEnds))
+//        {
+//            return response()->json([
+//                'status' => false,
+//                'error'  => "Aren't you supposed to be at office now?"
+//            ], Response::HTTP_LOCKED);
+//        }
 
-        if (!$curTime->clone()->isFriday() && $curTime->clone()->between($officeStarts, $officeEnds))
-        {
-            return response()->json([
-                'status' => false,
-                'error'  => "Aren't you supposed to be at office now?"
-            ], Response::HTTP_LOCKED);
-        }
-
-        $storiesAddedToday = auth()->user()->stories()->whereDate('created_at', $curTime->clone()->format('Y-m-d'))->count();
-
-        if ($storiesAddedToday == 3)
-        {
-            return response()->json([
-                'status' => false,
-                'error'  => 'You cannot upload more than 3 stories in one day.'
-            ], Response::HTTP_FORBIDDEN);
-        }
+//        $storiesAddedToday = auth()->user()->stories()->whereDate('created_at', $curTime->clone()->format('Y-m-d'))->count();
+//
+//        if ($storiesAddedToday == 3)
+//        {
+//            return response()->json([
+//                'status' => false,
+//                'error'  => 'You cannot upload more than 3 stories in one day.'
+//            ], Response::HTTP_FORBIDDEN);
+//        }
 
         return $next($request);
     }
