7f78c3443cbdc54ce4a60844332e28cd52271a19
nnawar706 <nnawar706@gmail.com>
2024-02-10 12:05:29 +0600

    ffprobe issue

--- app/Rules/UserStoryVideoDurationRule.php
+++ app/Rules/UserStoryVideoDurationRule.php
@@ -5,6 +5,7 @@ namespace App\Rules;
 use Closure;
 use FFMpeg\FFMpeg;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Owenoj\LaravelGetId3\GetId3;
 
 class UserStoryVideoDurationRule implements ValidationRule
 {
@@ -20,10 +21,8 @@ class UserStoryVideoDurationRule implements ValidationRule
         if ($extension == 'mp4') {
             try {
                 $ffmpeg = FFMpeg::create([
-                    'ffmpeg.binaries'  => exec('which ffmpeg'),
-                    'ffprobe.binaries' => exec('which ffprobe'),
-                    'timeout'          => 3600, // the timeout for the underlying process
-                    'ffmpeg.threads'   => 12,   // the number of threads that FFMpeg should use
+//                    'ffmpeg.binaries'  => exec('which ffmpeg'),
+                    'ffprobe.binaries' => '/tmp/ffprobe',
                 ]);
 
                 $video = $ffmpeg->open($value->getRealPath());
@@ -32,6 +31,14 @@ class UserStoryVideoDurationRule implements ValidationRule
                 if ($duration > 30) {
                     $fail('Uploaded video must be less than 30 seconds in duration.');
                 }
+
+//                $video = new GetId3($value);
+//
+//                $duration = $video->getPlaytime();
+//
+//                if ($duration > 30) {
+//                    $fail('Uploaded video must be less than 30 seconds in duration.');
+//                }
             } catch (\Throwable $th) {
                 $fail($th->getMessage());
             }
